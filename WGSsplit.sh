ce3b358ae62be1c9b0fcfe299253c382dc6ece95
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-19 22:19:16 +0800

    split the bam

--- WGSsplit.sh
+++ WGSsplit.sh
@@ -11,6 +11,9 @@ for (( i = 0; i < 24; i++ )); do
 	samtools index $chr${aNUMlist[i]}$bam
 done
 for (( i = 0; i < 24; i++ )); do
+	samtools view -b -h R19026468LD01-T0501-MOCK_sorted_dedup_realign.bam $chr$${aNUMlist[i]} > $chr${aNUMlist[i]}$con$bam
+done
+for (( i = 0; i < 24; i++ )); do
 	echo $chr${aNUMlist[i]}$con$bam
 	samtools index $chr${aNUMlist[i]}$con$bam
 done
