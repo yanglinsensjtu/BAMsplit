71735ab216a937f90f8928b9388d712031be93b4
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-19 22:55:36 +0800

    fix

--- WGSsplit.sh
+++ WGSsplit.sh
@@ -6,9 +6,10 @@ echo ${nums[*]}
 chr=chr
 bam='.bam'
 con=con
+
 for (( i = 0; i < 24; i++ )); do
 	echo $chr${nums[i]}$bam
-	samtools view -b -h R19026469LD01-T0502-CAR_sorted_dedup_realign.bam $chr$${nums[i]} > $chr${nums[i]}$bam
+	samtools view -b -h R19026469LD01-T0502-CAR_sorted_dedup_realign.bam $chr${nums[i]} > $chr${nums[i]}$bam
 done
 for (( i = 0; i < 24; i++ )); do
 	echo $chr${nums[i]}$bam
@@ -16,7 +17,7 @@ for (( i = 0; i < 24; i++ )); do
 done
 for (( i = 0; i < 24; i++ )); do
 	echo $chr${nums[i]}$con$bam
-	samtools view -b -h R19026468LD01-T0501-MOCK_sorted_dedup_realign.bam $chr$${nums[i]} > $chr${nums[i]}$con$bam
+	samtools view -b -h R19026468LD01-T0501-MOCK_sorted_dedup_realign.bam $chr${nums[i]} > $chr${nums[i]}$con$bam
 done
 for (( i = 0; i < 24; i++ )); do
 	echo $chr${nums[i]}$con$bam
