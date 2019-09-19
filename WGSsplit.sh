e076e12d112fd8cfaecc28ddb22deb893e8713e0
yanglinsensjtu <yanglinsensjtu@126.com>
2019-09-19 22:49:43 +0800

    fix

--- WGSsplit.sh
+++ WGSsplit.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-aNUMlist=($(seq 22))
-aNUMlist[22]=X
-aNUMlist[23]=Y
-echo ${aNUMlist[*]}
+nums=($(seq 22))
+nums[22]=X
+nums[23]=Y
+echo ${nums[*]}
 chr=chr
 bam='.bam'
 con=con
 for (( i = 0; i < 24; i++ )); do
-	echo $chr${aNUMlist[i]}$bam
-	samtools view -b -h R19026469LD01-T0502-CAR_sorted_dedup_realign.bam $chr$${aNUMlist[i]} > $chr${aNUMlist[i]}$bam
+	echo $chr${nums[i]}$bam
+	samtools view -b -h R19026469LD01-T0502-CAR_sorted_dedup_realign.bam $chr$${nums[i]} > $chr${nums[i]}$bam
 done
 for (( i = 0; i < 24; i++ )); do
-	echo $chr${aNUMlist[i]}$bam
-	samtools index $chr${aNUMlist[i]}$bam
+	echo $chr${nums[i]}$bam
+	samtools index $chr${nums[i]}$bam
 done
 for (( i = 0; i < 24; i++ )); do
-	echo $chr${aNUMlist[i]}$con$bam
-	samtools view -b -h R19026468LD01-T0501-MOCK_sorted_dedup_realign.bam $chr$${aNUMlist[i]} > $chr${aNUMlist[i]}$con$bam
+	echo $chr${nums[i]}$con$bam
+	samtools view -b -h R19026468LD01-T0501-MOCK_sorted_dedup_realign.bam $chr$${nums[i]} > $chr${nums[i]}$con$bam
 done
 for (( i = 0; i < 24; i++ )); do
-	echo $chr${aNUMlist[i]}$con$bam
-	samtools index $chr${aNUMlist[i]}$con$bam
+	echo $chr${nums[i]}$con$bam
+	samtools index $chr${nums[i]}$con$bam
 done
